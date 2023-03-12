89eb14441a195bf99db2e183475655abbcfd878f
ShoheiTakino <shohei0522penguin@gmail.com>
2023-03-12 17:49:33 +0900

    詳細画面のviewを切り出し

---
+++ SwiftUI+MVPByPokemon/Presentation/PokemonDetail/UI/PokemonDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonDetailView.swift
+//  SwiftUI+MVPByPokemon
+//
+//  Created by 滝野翔平 on 2023/03/12.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    let number: Int
+    var body: some View {
+        Text("\(number)")
+            .padding()
+    }
+}
